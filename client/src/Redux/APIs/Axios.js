4a1d31d4a39479106f5963c515ed94fa25a2577d
amireinidavid <realheritagecompany@gmail.com>
2024-08-08 19:41:18 +0100

    Update Axios.js

--- client/src/Redux/APIs/Axios.js
+++ client/src/Redux/APIs/Axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Axios = axios.create({
-  baseURL: "https://daveflix-admin-nn1d-ihva1rafd-david-amires-projects.vercel.app/",
+  baseURL: "https://daveflix-admin-nn1d.vercel.app/api",
 });
 
 export default Axios;
