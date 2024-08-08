b2a477fd7624ed6dd716161d0f9d70eb01488b5a
amireinidavid <realheritagecompany@gmail.com>
2024-08-08 19:38:39 +0100

    axios

--- client/src/Redux/APIs/Axios.js
+++ client/src/Redux/APIs/Axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Axios = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://daveflix-admin-nn1d.vercel.app/api",
 });
 
 export default Axios;
